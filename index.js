37f01c7c8d8ea657bbcb10ed34d88db15f947ed8
Sayantan Dutta <sayantanduttasd1999@gmail.com>
2021-06-08 13:55:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ window.onload=()=>{
   let song = document.getElementById("audio");
   song.play();
   song.loop=true;
+    document.getElementsByClassName("home")[0].style.display = "block";
+  document.getElementsByClassName("seasons")[0].style.display = "none";
+    document.getElementsByClassName("real-characters")[0].style.display = "none";
+    document.getElementsByClassName("productions")[0].style.display = "none";
+    document.getElementsByClassName("shows1")[0].style.display = "none";
+    document.getElementsByClassName("stars")[0].style.display = "none";
 
-
-  $(".home").show();
-  $(".seasons").hide();
-    $(".real-characters").hide();
-    $(".production").hide();
-    $(".shows1").hide();
-    $(".stars").hide();
+//   $(".home").show();
+//   $(".seasons").sty;
+//     $(".real-characters").hide();
+//     $(".production").hide();
+//     $(".shows1").hide();
+//     $(".stars").hide();
 
 }
 
@@ -79,12 +84,12 @@ function showNav() {
   document.getElementsByClassName("right-menu")[0].style.width = "60%";
 }
 
-// function closeNav(){
-//   document.getElementsByClassName("right-menu")[0].style.width = "0%";
-// }
+function closeNav(){
+  document.getElementsByClassName("right-menu")[0].style.width = "0%";
+}
 document.addEventListener('click', function(e) {
     var container = document.getElementById('rm');
-    if (!container.contains(e.target) || container.contains(e.target) ) {
+    if (!container.contains(e.target)) {
         document.getElementsByClassName("right-menu")[0].style.width = "0%";
     }
 });
